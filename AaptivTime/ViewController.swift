cd09225f18e8fb8fe3215d690972838453f9e557
wanghao <hao.wang@hacknocraft.com>
2018-02-06 23:33:34 -0500

    Initial Commit

---
+++ AaptivTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AaptivTime
+//
+//  Created by HAO WANG on 2/6/18.
+//  Copyright © 2018 HaoWang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
